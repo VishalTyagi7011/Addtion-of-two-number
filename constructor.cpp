0f1dcb715855e0fd0b6ec16dceb1baec404a87f1
VISHAL TYAGI <101706996+VishalTyagi7011@users.noreply.github.com>
2022-06-08 08:07:51 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class rohit
+{
+	//cout<<"hello world";
+	public:
+	rohit()
+	{
+		cout<<"constructor calling";
+		
+		void vishal()
+		{
+			cout<<"hello vishal";
+			return 0;
+		}
+	}
+	
+};
+int main()
+{
+	rohit object;
+	
+}
