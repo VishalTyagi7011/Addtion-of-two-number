12ae751222e6b7fb449cb781910da9d9dc3d9ab7
VISHAL TYAGI <101706996+VishalTyagi7011@users.noreply.github.com>
2022-06-08 08:06:50 +0530

    Add files via upload

---
+++ 5x5pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+		cout<<"*"<<"\t";
+		
+    
+    
+		for(int j=0;j<i;j++)
+		{
+			cout<<"*"<<"\t";
+		}
+		cout<<"\n";
+    }   
+	
+
+	return 0;
+}
